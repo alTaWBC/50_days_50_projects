edf6096aaa3e27319aa985b5fb5df196578c5a59
alTabWBC <wd.costa@campus.fct.unl.pt>
2021-02-21 14:53:00 +0000

    Day 3 progress steps

--- progress_steps/script.js
+++ progress_steps/script.js
@@ -2,11 +2,18 @@ const progress = document.getElementById("progress");
 const prev = document.getElementById("prev");
 const next = document.getElementById("next");
 const circles = document.querySelectorAll(".circle");
+console.log(circles);
 
 let currentActive = 1;
 
-next.addEventListener("click", () => (currentActive = Math.min(currentActive + 1, circles.length)));
-prev.addEventListener("click", () => (currentActive = Math.max(currentActive - 1, 0)));
+next.addEventListener("click", () => {
+    currentActive = Math.min(currentActive + 1, circles.length);
+    update();
+});
+prev.addEventListener("click", () => {
+    currentActive = Math.max(currentActive - 1, 0);
+    update();
+});
 
 const update = () => {
     circles.forEach((circle, index) => {
@@ -14,10 +21,14 @@ const update = () => {
         else circle.classList.remove("active");
     });
 
-    const actives = document.querySelectorAll("active");
-    const percentage = (actives.length - 1 / circle.length) * 100 + "%";
+    const actives = document.querySelectorAll(".active");
+    const percentage = ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
     progress.style.width = percentage;
 
+    console.log(actives);
+    console.log(actives.length - 1, circles.length);
+    console.log((actives.length - 1) / circles.length);
+
     prev.disabled = currentActive === 1;
-    prev.disabled = currentActive === circles.length;
+    next.disabled = currentActive === circles.length;
 };
